f263832cce9dca1d57234bd866e13c0e2f6d08e0
scottdharris <scottdharris11@yahoo.com>
2025-01-25 06:38:30 -0500

    day 23 part 1 using linked list

--- src/com/harris/AdventDay23.java
+++ src/com/harris/AdventDay23.java
@@ -2,114 +2,141 @@ package com.harris;
 
 import java.time.Duration;
 import java.time.Instant;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
+import java.util.*;
 
 public class AdventDay23 {
 
     public void executePart1() {
         System.out.println("ADVENT DAY 23 - PART 1...");
-        //List<Integer> cups = new ArrayList( Arrays.asList( 3,8,9,1,2,5,4,6,7 ) );
-        List<Integer> cups = new ArrayList( Arrays.asList( 8,7,1,3,6,9,4,5,2 ) );
+        //int[] labels = new int[]{ 3,8,9,1,2,5,4,6,7 };
+        int[] labels = new int[]{ 8,7,1,3,6,9,4,5,2 };
 
         Instant start = Instant.now();
-        int currentCup = 0;
+
+        CrabCups game = new CrabCups(labels, -1);
+        Cup current = game.getCup(labels[0]);
         for ( int i = 0; i < 100; i++ ) {
-            currentCup = playRound( currentCup, cups );
+            current = game.playRound(current);
         }
 
         StringBuilder sb = new StringBuilder();
-        int idxOfOne = cups.indexOf( 1 );
-        for ( int i = idxOfOne + 1; i < cups.size(); i++ ) {
-            sb.append( cups.get( i ) );
-        }
-        for ( int i = 0; i < idxOfOne; i++ ) {
-            sb.append( cups.get( i ) );
+        current = game.getCup(1).next;
+        while (current.label != 1) {
+            sb.append(current.label);
+            current = current.next;
         }
 
         Instant end = Instant.now();
-        System.out.println("  [" + Duration.between(start, end) + "] Answer: " + sb.toString() );
+        System.out.println("  [" + Duration.between(start, end) + "] Answer: " + sb );
     }
 
     public void executePart2() {
         System.out.println("ADVENT DAY 23 - PART 2...");
-        List<Integer> cups = new ArrayList( Arrays.asList( 3,8,9,1,2,5,4,6,7 ) );
-        //List<Integer> cups = new ArrayList( Arrays.asList( 8,7,1,3,6,9,4,5,2 ) );
+        //int[] labels = new int[]{ 3,8,9,1,2,5,4,6,7 };
+        //int[] labels = new int[]{ 8,7,1,3,6,9,4,5,2 };
 
         Instant start = Instant.now();
-        for ( int i = 10; i <= 100; i++ ) {
-            cups.add( i );
-        }
 
-        int currentCup = 0;
-        for ( int i = 0; i < 100; i++ ) {
-            if ( i % 1000 == 0 ) { System.out.println( "  Playing round: " + (i+1) ); }
-            currentCup = playRound( currentCup, cups );
-        }
-
-        int idxOfOne = cups.indexOf( 1 );
-        int firstValIdx = idxOfOne + 1;
-        int firstVal = firstValIdx < cups.size() ? cups.get( firstValIdx ) : cups.get( firstValIdx - cups.size() );
-        int secondValIdx = idxOfOne + 2;
-        int secondVal = secondValIdx < cups.size() ? cups.get( secondValIdx ) : cups.get( secondValIdx - cups.size() );
-        long answer = firstVal * secondVal;
+        long answer = 0;
 
         Instant end = Instant.now();
-        System.out.println("  [" + Duration.between(start, end) + "] WRONG Answer: " + answer );
+        System.out.println("  [" + Duration.between(start, end) + "] Answer: " + answer );
+    }
+}
+
+class Cup {
+    int label;
+    Cup next;
+    Cup prev;
+    public String toString() {
+        return String.valueOf(label);
     }
 
-    private int playRound( int currentCup, List<Integer> cups ) {
-        // Get the current cup value
-        int currentCupLbl = cups.get( currentCup );
-
-        // Remove three cards left of current cup
-        List<Integer> removed = new ArrayList<>();
-        int removeIdx = currentCup + 1;
-        for ( int i = 0; i < 3; i++ ) {
-            if ( removeIdx >= cups.size() ) {
-                removeIdx = 0;
-            }
-            removed.add( cups.remove( removeIdx ) );
-        }
-
-        // Find the destination index (current cup label - 1 or next in line)
-        int destinationLbl = currentCupLbl - 1;
-        int destinationIdx = -1;
-        while ( destinationIdx < 0 ) {
-            int minLbl = Integer.MAX_VALUE;
-            int maxLbl = Integer.MIN_VALUE;
-            for ( int i = 0; i < cups.size(); i++ ) {
-                int cupLbl = cups.get( i );
-                if ( cupLbl < minLbl ) { minLbl = cupLbl; }
-                if ( cupLbl > maxLbl ) { maxLbl = cupLbl; }
-                if ( cupLbl == destinationLbl ) {
-                    destinationIdx = i;
-                    break;
-                }
-            }
-
-            if ( destinationIdx < 0 ) {
-                destinationLbl--;
-                if ( destinationLbl < minLbl ) {
-                    destinationLbl = maxLbl;
-                }
-            }
-        }
-
-        // place cups back in the list at the destination index
-        destinationIdx++;
-        for ( Integer r : removed ) {
-            cups.add( destinationIdx, r );
-            destinationIdx++;
-        }
-
-        // Return index of next cup
-        int nextCup = cups.indexOf( currentCupLbl ) + 1;
-        if ( nextCup >= cups.size() ) {
-            nextCup = 0;
-        }
-        return nextCup;
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Cup cup = (Cup) o;
+        return label == cup.label;
     }
 
+    @Override
+    public int hashCode() {
+        return label;
+    }
+}
+
+class CrabCups {
+    private final Map<Integer,Cup> cups = new HashMap<>();
+    private int minLabel;
+    private int maxLabel;
+
+    CrabCups(int[] labels, int last) {
+        Cup first = new Cup();
+        first.label = labels[0];
+        cups.put(first.label, first);
+        Cup prev = first;
+        minLabel = labels[0];
+        maxLabel = labels[0];
+        for (int i = 1; i < labels.length; i++) {
+            Cup c = new Cup();
+            c.label = labels[i];
+            cups.put(c.label, c);
+            c.prev = prev;
+            prev.next = c;
+            prev = c;
+            if (labels[i] < minLabel) {
+                minLabel = labels[i];
+            }
+            if (labels[i] > maxLabel) {
+                maxLabel = labels[i];
+            }
+        }
+        for (int label = maxLabel + 1; label <= last; label++) {
+            Cup c = new Cup();
+            c.label = label;
+            cups.put(c.label, c);
+            c.prev = prev;
+            prev.next = c;
+            prev = c;
+            maxLabel = label;
+        }
+        prev.next = first;
+        first.prev = prev;
+    }
+
+    Cup getCup(int label) {
+        return cups.get(label);
+    }
+
+    Cup playRound(Cup start) {
+        // select next 3 cups clockwise from start
+        Cup[] selected = new Cup[3];
+        selected[0] = start.next;
+        selected[1] = selected[0].next;
+        selected[2] = selected[1].next;
+
+        // determine destination cup based on cup with a label
+        // that is one minus the starting label, so long as it
+        // is not a selected cup. wrap from min to max if necessary.
+        int dest = start.label;
+        do {
+            dest--;
+            if (dest < minLabel) {
+                dest = maxLabel;
+            }
+        } while (dest == selected[0].label || dest == selected[1].label || dest == selected[2].label);
+        Cup destCup = cups.get(dest);
+
+        // move selected
+        start.next = selected[2].next;
+        selected[2].next = destCup.next;
+        selected[2].next.prev = selected[2];
+        destCup.next = selected[0];
+        selected[0].prev = destCup;
+        start.next.prev = start;
+
+        // return the next cup after the start
+        return start.next;
+    }
 }
